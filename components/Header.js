b5f60417f7aae2ff92acfb420fc6ed74fbd06237
Rajdeep Ghosh <rajdeepghosh810@gmail.com>
2021-07-08 14:50:41 +0530

    Add Grid icon

--- components/Header.js
+++ components/Header.js
@@ -1,6 +1,8 @@
+import { ViewGridIcon } from "@heroicons/react/solid";
+
 function Header() {
   return (
-    <div className="flex justify-between p-5">
+    <div className="flex justify-between items-center p-5">
       <div className="flex space-x-6">
         <p className="cursor-pointer hover:underline hover:text-gray-900">About</p>
         <p className="cursor-pointer hover:underline hover:text-gray-900">Store</p>
@@ -8,6 +10,7 @@ function Header() {
       <div className="flex space-x-6">
         <p className="cursor-pointer hover:underline hover:text-gray-900">Gmail</p>
         <p className="cursor-pointer hover:underline hover:text-gray-900">Images</p>
+        <ViewGridIcon className="h-6 cursor-pointer" />
       </div>
     </div>
   );
