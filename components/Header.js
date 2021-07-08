fff3d26dbe5da43a58a571b956e3e9cb974e1a8f
Rajdeep Ghosh <rajdeepghosh810@gmail.com>
2021-07-08 14:25:25 +0530

    Populate Header component

--- components/Header.js
+++ components/Header.js
@@ -1,5 +1,16 @@
 function Header() {
-  return <div>This is the header</div>;
+  return (
+    <div className="flex justify-between p-5 ">
+      <div className="flex space-x-6">
+        <p>About</p>
+        <p>Store</p>
+      </div>
+      <div className="flex space-x-6">
+        <p>Gmail</p>
+        <p>Images</p>
+      </div>
+    </div>
+  );
 }
 
 export default Header;
