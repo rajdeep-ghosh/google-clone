0c3cbf1e7c95ba6f9bc43256d4d962ba2723eadc
Rajdeep Ghosh <rajdeepghosh810@gmail.com>
2021-07-08 14:41:03 +0530

    Style Header

--- components/Header.js
+++ components/Header.js
@@ -1,13 +1,13 @@
 function Header() {
   return (
-    <div className="flex justify-between p-5 ">
+    <div className="flex justify-between p-5">
       <div className="flex space-x-6">
-        <p>About</p>
-        <p>Store</p>
+        <p className="cursor-pointer hover:underline hover:text-gray-900">About</p>
+        <p className="cursor-pointer hover:underline hover:text-gray-900">Store</p>
       </div>
       <div className="flex space-x-6">
-        <p>Gmail</p>
-        <p>Images</p>
+        <p className="cursor-pointer hover:underline hover:text-gray-900">Gmail</p>
+        <p className="cursor-pointer hover:underline hover:text-gray-900">Images</p>
       </div>
     </div>
   );
