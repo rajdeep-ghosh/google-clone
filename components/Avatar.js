190a5622f69a895e6982ec12d91db4aa2c57d3b7
Rajdeep Ghosh <rajdeepghosh810@gmail.com>
2021-07-08 19:18:44 +0530

    Add Avatar

---
+++ components/Avatar.js
@@ -0,0 +1,15 @@
+function Avatar() {
+  return (
+    // eslint-disable-next-line @next/next/no-img-element
+    <img
+      className="bg-red-500 rounded-full"
+      src="/avatar/panda.png"
+      width={33}
+      height={33}
+      loading="lazy"
+      alt="avatar"
+    />
+  );
+}
+
+export default Avatar;
--- components/Header.js
+++ components/Header.js
@@ -1,18 +1,20 @@
 import { ViewGridIcon } from "@heroicons/react/solid";
+import Avatar from "./Avatar";
 
 function Header() {
   return (
-    <div className="flex justify-between items-center p-5">
-      <div className="flex space-x-6">
+    <header className="flex justify-between items-center p-5">
+      <div className="flex space-x-6 items-center">
         <p className="cursor-pointer hover:underline hover:text-gray-900">About</p>
         <p className="cursor-pointer hover:underline hover:text-gray-900">Store</p>
       </div>
-      <div className="flex space-x-6">
+      <div className="flex space-x-6 items-center">
         <p className="cursor-pointer hover:underline hover:text-gray-900">Gmail</p>
         <p className="cursor-pointer hover:underline hover:text-gray-900">Images</p>
         <ViewGridIcon className="h-6 cursor-pointer" />
+        <Avatar />
       </div>
-    </div>
+    </header>
   );
 }
 
