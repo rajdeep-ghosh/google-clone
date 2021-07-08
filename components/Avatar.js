c194103096db0eff95a4c77cf73ea998f4bb8574
Rajdeep Ghosh <rajdeepghosh810@gmail.com>
2021-07-08 19:44:48 +0530

    Add animation to avatar

--- components/Avatar.js
+++ components/Avatar.js
@@ -2,7 +2,7 @@ function Avatar() {
   return (
     // eslint-disable-next-line @next/next/no-img-element
     <img
-      className="bg-red-500 rounded-full cursor-pointer"
+      className="bg-red-500 rounded-full cursor-pointer transform transition duration-150 hover:scale-110"
       src="/avatar/panda.png"
       width={33}
       height={33}
