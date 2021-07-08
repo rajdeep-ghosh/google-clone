13adb4e3fa828c66cb7a791cf2c31468b3fea1b9
Rajdeep Ghosh <rajdeepghosh810@gmail.com>
2021-07-09 00:52:41 +0530

    Add dark mode

--- components/Header.js
+++ components/Header.js
@@ -5,6 +5,12 @@ import Avatar from "./Avatar";
 function Header() {
   const [isDarkMode, setDarkMode] = useState(false);
 
+  function handleTheme() {
+    const root = window.document.documentElement;
+    root.classList.toggle('dark');
+    setDarkMode(!isDarkMode);
+  }
+
   return (
     <header className="flex justify-between items-center p-5">
       <div className="flex space-x-6 items-center">
@@ -12,7 +18,7 @@ function Header() {
         <p className="cursor-pointer hover:underline hover:text-gray-900">Store</p>
       </div>
       <div className="flex space-x-6 items-center">
-        <div onClick={() => {setDarkMode(!isDarkMode)}}>
+        <div onClick={handleTheme}>
           {isDarkMode ? <SunIcon className="h-6 cursor-pointer" /> : <MoonIcon className="h-6 cursor-pointer" />}
         </div>
         <p className="cursor-pointer hover:underline hover:text-gray-900">Gmail</p>
