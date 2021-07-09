b7290c74895d5e8ad2576d635cc483e17c335341
Rajdeep Ghosh <rajdeepghosh810@gmail.com>
2021-07-10 03:17:40 +0530

    Add languages offered

--- components/Main.js
+++ components/Main.js
@@ -13,11 +13,23 @@ function Main({ isDarkMode }) {
         <div className="searchbar">
           <SearchIcon className="h-5 mr-3 text-gray-500 dark:text-gray-400" />
           <input className="flex-grow focus:outline-none dark:bg-darkmode" type="text" />
-          <MicrophoneIcon className="h-5 ml-3 text-gray-500 dark:text-gray-400" />
+          <MicrophoneIcon className="h-5 ml-3 cursor-pointer text-gray-500 dark:text-gray-400" />
         </div>
         <div className="flex space-x-3">
           <button className="btn">Google Search</button>
           <button className="btn">I'm Feeling Lucky</button>
+        </div>
+        <div className="flex space-x-3 mt-7 text-sm">
+          <p className="dark:text-gray-400">Google offered in:</p>
+          <p className="text-blue-500 hover:underline cursor-pointer">বাংলা</p>
+          <p className="text-blue-500 hover:underline cursor-pointer">हिन्दी</p>
+          <p className="text-blue-500 hover:underline cursor-pointer">मराठी</p>
+          <p className="text-blue-500 hover:underline cursor-pointer">français</p>
+          <p className="text-blue-500 hover:underline cursor-pointer">español</p>
+          <p className="text-blue-500 hover:underline cursor-pointer">日本語</p>
+          <p className="text-blue-500 hover:underline cursor-pointer">Deutsche</p>
+          <p className="text-blue-500 hover:underline cursor-pointer">português</p>
+          <p className="text-blue-500 hover:underline cursor-pointer">русский</p>
         </div>
       </form>
     </main>
