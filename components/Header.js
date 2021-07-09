70ad311af71412aa7efbf565e44f95394c68bece
Rajdeep Ghosh <rajdeepghosh810@gmail.com>
2021-07-10 05:00:48 +0530

    Make mobile responsive

--- components/Header.js
+++ components/Header.js
@@ -5,15 +5,14 @@ function Header({ changeTheme, isDarkMode }) {
   return (
     <header className="flex justify-between items-center p-5 w-full">
       <div className="flex space-x-6 items-center">
-        <p className="cursor-pointer hover:underline hover:text-gray-900 dark:hover:text-white">About</p>
-        <p className="cursor-pointer hover:underline hover:text-gray-900 dark:hover:text-white">Store</p>
+        <p className="cursor-pointer hover:underline hover:text-gray-900 dark:hover:text-white hidden sm:block">Store</p>
       </div>
       <div className="flex space-x-6 items-center">
         <div onClick={changeTheme}>
           {isDarkMode ? <SunIcon className="h-6 cursor-pointer" /> : <MoonIcon className="h-6 cursor-pointer" />}
         </div>
-        <p className="cursor-pointer hover:underline hover:text-gray-900 dark:hover:text-white">Gmail</p>
-        <p className="cursor-pointer hover:underline hover:text-gray-900 dark:hover:text-white">Images</p>
+        <p className="cursor-pointer hover:underline hover:text-gray-900 dark:hover:text-white hidden sm:block">Gmail</p>
+        <p className="cursor-pointer hover:underline hover:text-gray-900 dark:hover:text-white hidden sm:block">Images</p>
         <ViewGridIcon className="h-6 cursor-pointer" />
         <Avatar />
       </div>
