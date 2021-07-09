3832c0b160c12a3451c1fff861022050b4a88a61
Rajdeep Ghosh <rajdeepghosh810@gmail.com>
2021-07-09 16:48:58 +0530

    Style searchbar

--- components/Main.js
+++ components/Main.js
@@ -1,4 +1,5 @@
 /* eslint-disable @next/next/no-img-element */
+import { MicrophoneIcon, SearchIcon } from "@heroicons/react/solid";
 
 function Main({ isDarkMode }) {
   const logo = `/logo/googlelogo-${isDarkMode ? `light` : `color`}.png`;
@@ -7,6 +8,11 @@ function Main({ isDarkMode }) {
     <main>
       <form>
         <img src={logo} loading="eager" alt="logo" />
+        <div className="searchbar">
+          <SearchIcon className="h-5 mr-3 text-gray-500 dark:text-gray-400" />
+          <input className="flex-grow focus:outline-none dark:bg-darkmode" type="text" />
+          <MicrophoneIcon className="h-5 ml-3 text-gray-500 dark:text-gray-400" />
+        </div>
       </form>
     </main>
   );
