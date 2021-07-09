a305fe02d0afca08be367bdae3438c0e3a233937
Rajdeep Ghosh <rajdeepghosh810@gmail.com>
2021-07-10 05:23:06 +0530

    Underline footer text on hover

--- components/Footer.js
+++ components/Footer.js
@@ -2,19 +2,19 @@ function Footer() {
   return (
     <footer className="footer absolute bottom-0">
       <div className="mb-3">
-          <p>India</p>
+        <p>India</p>
       </div>
       <div className="flex flex-col sm:flex-row items-center justify-between">
-          <div className="flex space-x-5 mt-3">
-              <a href="https://about.google/" target="_blank" rel="noreferrer">About</a>
-              <a href="https://www.google.com/services/" target="_blank" rel="noreferrer">Business</a>
-              <a href="https://google.com/search/howsearchworks/" target="_blank" rel="noreferrer">How Search Works</a>
-          </div>
-          <div className="flex space-x-5 mt-3">
-              <a href="https://policies.google.com/privacy?hl=en-IN&fg=1" target="_blank" rel="noreferrer">Privacy</a>
-              <a href="https://policies.google.com/terms?hl=en-IN&fg=1" target="_blank" rel="noreferrer">Terms</a>
-              <p>Settings</p>
-          </div>
+        <div className="flex space-x-5 mt-3">
+          <a className="hover:underline" href="https://about.google/" target="_blank" rel="noreferrer">About</a>
+          <a className="hover:underline" href="https://www.google.com/services/" target="_blank" rel="noreferrer">Business</a>
+          <a className="hover:underline" href="https://google.com/search/howsearchworks/" target="_blank" rel="noreferrer">How Search Works</a>
+        </div>
+        <div className="flex space-x-5 mt-3">
+          <a className="hover:underline" href="https://policies.google.com/privacy?hl=en-IN&fg=1" target="_blank" rel="noreferrer">Privacy</a>
+          <a className="hover:underline" href="https://policies.google.com/terms?hl=en-IN&fg=1" target="_blank" rel="noreferrer">Terms</a>
+          <p className="hover:underline cursor-pointer">Settings</p>
+        </div>
       </div>
     </footer>
   );
