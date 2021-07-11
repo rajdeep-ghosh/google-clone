162c55936e4febf4ab00efcb104edf07ef191d57
Rajdeep Ghosh <rajdeepghosh810@gmail.com>
2021-07-11 17:00:28 +0530

    Link google web store

--- components/Header.js
+++ components/Header.js
@@ -5,7 +5,13 @@ function Header({ changeTheme, isDarkMode }) {
   return (
     <header className="flex justify-between items-center absolute top-0 p-5 w-full">
       <div className="flex space-x-6 items-center">
-        <p className="cursor-pointer hover:underline hover:text-gray-900 dark:hover:text-white hidden sm:block">Store</p>
+        <a
+          href="https://chrome.google.com/webstore/"
+          className="cursor-pointer hover:underline hover:text-gray-900 dark:hover:text-white hidden sm:block"
+          rel="noreferrer"
+        >
+          Store
+        </a>
       </div>
       <div className="flex space-x-6 items-center">
         <div onClick={changeTheme}>
