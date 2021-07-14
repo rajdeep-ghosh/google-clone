1c9ec6b03fa84657809f5f339b8c688dfc8e7084
Rajdeep Ghosh <rajdeepghosh810@gmail.com>
2021-07-14 20:57:35 +0530

    Create SearchHeader component

---
+++ components/SearchHeader.js
@@ -0,0 +1,5 @@
+function SearchHeader() {
+  return <div>Test</div>;
+}
+
+export default SearchHeader;
--- pages/search.js
+++ pages/search.js
@@ -1,5 +1,21 @@
+import Head from "next/head";
+import SearchHeader from "../components/SearchHeader";
+
 function Search() {
-  return <div>hello</div>;
+  return (
+    <div>
+      <Head>
+        <title>Search Results - Google Clone Search</title>
+        <meta name="description" content="Google Clone built using NEXT.JS and Tailwind CSS" />
+        <link rel="apple-touch-icon" sizes="180x180" href="favicon/apple-touch-icon.png" />
+        <link rel="icon" type="image/png" sizes="32x32" href="favicon/favicon-32x32.png" />
+        <link rel="icon" type="image/png" sizes="16x16" href="favicon/favicon-16x16.png" />
+        <link rel="manifest" href="favicon/site.webmanifest" />
+      </Head>
+
+      <SearchHeader />
+    </div>
+  );
 }
 
 export default Search;
