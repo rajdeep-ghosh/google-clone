ea71a36085729dfc9367f03c9080a34da75c56ff
Rajdeep Ghosh <rajdeepghosh810@gmail.com>
2021-07-23 02:43:37 +0530

    Add copyright

--- components/Footer.js
+++ components/Footer.js
@@ -1,20 +1,28 @@
 function Footer() {
   return (
     <footer className="footer absolute bottom-0">
-      <div className="mb-3">
+      <div className="mb-2">
         <p>India</p>
       </div>
       <div className="flex flex-col sm:flex-row items-center justify-between">
-        <div className="flex space-x-5 mt-3">
+        <div className="flex space-x-5 mt-2">
           <a className="hover:underline" href="https://about.google/" target="_blank" rel="noreferrer">About</a>
           <a className="hover:underline" href="https://www.google.com/services/" target="_blank" rel="noreferrer">Business</a>
           <a className="hover:underline" href="https://google.com/search/howsearchworks/" target="_blank" rel="noreferrer">How Search Works</a>
         </div>
-        <div className="flex space-x-5 mt-3">
+        <div className="flex space-x-5 mt-2">
           <a className="hover:underline" href="https://policies.google.com/privacy?hl=en-IN&fg=1" target="_blank" rel="noreferrer">Privacy</a>
           <a className="hover:underline" href="https://policies.google.com/terms?hl=en-IN&fg=1" target="_blank" rel="noreferrer">Terms</a>
           <p className="hover:underline cursor-pointer">Settings</p>
         </div>
+      </div>
+      <div className="flex justify-center mt-2">
+        <p className="mt-2">
+          &copy; Rajdeep Ghosh 2021 |&nbsp;
+          <a href="https://github.com/rajdeep-ghosh/google-clone/" target="_blank" rel="noreferrer">GitHub</a>
+          &nbsp;|&nbsp;
+          <a href="https://linkedin.com/in/rajdeepghosh01/" target="_blank" rel="noreferrer">LinkedIn</a>
+        </p>
       </div>
     </footer>
   );
