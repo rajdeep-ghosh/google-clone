3be9ca774d5e9b73a06fd59e089f9063e9680e14
Rajdeep Ghosh <rajdeepghosh810@gmail.com>
2021-08-05 15:03:14 +0530

    Use dummy data

--- pages/search.js
+++ pages/search.js
@@ -1,6 +1,7 @@
 import { useState } from "react";
 import Head from "next/head";
 import SearchHeader from "../components/SearchHeader";
+import dummyResponse from "../dummyResponse";
 
 function Search({ results }) {
   console.log(results);
@@ -33,9 +34,11 @@ export default Search;
 export async function getServerSideProps(context) {
   const API_KEY = process.env.SEARCH_API_KEY;
   const CX_KEY = process.env.CONTEXT_KEY;
-  const useDummyData = false;
+  const useDummyData = true;
 
-  const data = await fetch(
+  const data = useDummyData 
+  ? dummyResponse 
+  : await fetch(
     `https://www.googleapis.com/customsearch/v1?key=${API_KEY}&cx=${CX_KEY}&q=${context.query.query}`
   ).then(response => response.json());
 
