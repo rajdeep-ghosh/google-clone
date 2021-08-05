34b910c89ad27db6782e6f1b222a1d200258abf2
Rajdeep Ghosh <rajdeepghosh810@gmail.com>
2021-08-05 19:02:22 +0530

    Add search time and quantity

--- components/SearchResults.js
+++ components/SearchResults.js
@@ -1,9 +1,9 @@
 function SearchResults({ results }) {
   return (
-    <div>
-      <p>
-        About {results.searchInformation.formattedTotalResults} results (
-        {results.searchInformation.formattedSearchTime} seconds)
+    <div className="mx-auto w-full px-9 sm:pl-[8%] md:pl-[14%] lg:pl-[180px]">
+      <p className="text-gray-500 dark:text-gray-400 text-sm mt-3">
+        About {results.searchInformation?.formattedTotalResults} results (
+        {results.searchInformation?.formattedSearchTime} seconds)
       </p>
     </div>
   );
