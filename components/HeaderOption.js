7811d5af978cb53b0f9a5cebf736ce0089d3d199
Rajdeep Ghosh <rajdeepghosh810@gmail.com>
2021-08-05 13:42:07 +0530

    Complete HearderOption

--- components/HeaderOption.js
+++ components/HeaderOption.js
@@ -1,8 +1,10 @@
 function HeaderOption({ Icon, title, selected }) {
   return (
-    <div>
+    <div
+      className={`flex items-center space-x-1 border-b-4 border-transparent hover:border-blue-500 hover:text-blue-500 pb-3 cursor-pointer ${selected && "text-blue-500 border-blue-500"}`}
+    >
       <Icon className="h-5" />
-      <p>{title}</p>
+      <p className="hidden sm:inline-block">{title}</p>
     </div>
   );
 }
